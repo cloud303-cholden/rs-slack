20153adb32edf773434102ceb3d94e22845a9f0a
Conner Holden <80974257+cloud303-cholden@users.noreply.github.com>
2023-12-08 17:38:36 -0700

    Add `PostMessageOutput`

--- src/api/chat.rs
+++ src/api/chat.rs
@@ -1,7 +1,8 @@
 use reqwest::header::{HeaderMap, AUTHORIZATION, CONTENT_TYPE};
-use serde_json::json;
 
 use crate::client::{FromClient, Client};
+
+use super::Api;
 
 pub struct Chat<'a> {
     client: &'a reqwest::Client,
@@ -12,17 +13,23 @@ impl<'a> Chat<'a> {
     pub async fn post_message(
         &self,
         input: input::PostMessage<'a>,
-    ) -> Result<reqwest::Response, reqwest::Error> {
-        let body = json!({"text": input.text, "channel": input.channel, "thread_ts": input.thread_ts});
+    ) -> Result<output::PostMessage, reqwest::Error> {
         let mut headers = HeaderMap::new();
         headers.append(AUTHORIZATION, format!("Bearer {}", self.token).parse().unwrap());
-        headers.append(CONTENT_TYPE, "application/json".parse().unwrap());
-        self.client
+        headers.append(CONTENT_TYPE, "application/json; charset=utf-8".parse().unwrap());
+        let resp: output::PostMessage = self.client
             .post("https://slack.com/api/chat.postMessage")
-            .json(&body)
+            .json(&input)
             .headers(headers)
-            .send()
-            .await
+            .send().await?
+            .json().await?;
+        Ok(resp)
+    }
+}
+
+impl<'a> Api<'a> for Chat<'a> {
+    fn endpoint() -> &'a str {
+       "https://slack.com/api/chat.postMessage" 
     }
 }
 
@@ -36,11 +43,81 @@ impl<'a> FromClient<'a> for Chat<'a> {
 }
 
 pub mod input {
-    #[derive(Default)]
+    #[derive(Default, serde::Serialize)]
     pub struct PostMessage<'a> {
         pub channel: &'a str,
         pub text: &'a str,
         pub thread_ts: Option<&'a str>,
     }
     pub type PostMessageInput<'a> = PostMessage<'a>;
+}
+
+pub mod output {
+    use crate::api::chat::types::*;
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct PostMessage {
+        pub ok: bool,
+        pub channel: String,
+        pub ts: String,
+        pub message: Message,
+        pub warning: Option<String>,
+        pub response_metadata: Option<ResponseMetadata>,
+    }
+    pub type PostMessageOutput = PostMessage;
+}
+
+pub mod types {
+    #[derive(Debug, serde::Deserialize)]
+    pub struct Message {
+        #[serde(rename(deserialize = "type"))]
+        pub type_: String,
+        pub bot_id: String,
+        pub text: String,
+        pub user: String,
+        pub ts: String,
+        pub app_id: String,
+        pub blocks: Vec<Block>,
+        pub team: String,
+        pub bot_profile: BotProfile,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct Block {
+        #[serde(rename(deserialize = "type"))]
+        pub type_: String,
+        pub block_id: String,
+        pub elements: Vec<Element>,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct Element {
+        #[serde(rename(deserialize = "type"))]
+        pub type_: String,
+        pub elements: Option<Vec<Element>>,
+        pub text: Option<String>,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct BotProfile {
+        pub id: String,
+        pub app_id: String,
+        pub name: String,
+        pub icons: Icons,
+        pub deleted: bool,
+        pub updated: u64,
+        pub team_id: String,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct Icons {
+        pub image_36: String,
+        pub image_48: String,
+        pub image_72: String,
+    }
+
+    #[derive(Debug, serde::Deserialize)]
+    pub struct ResponseMetadata {
+        pub warnings: Vec<String>,
+    }
 }
--- src/api/mod.rs
+++ src/api/mod.rs
@@ -4,3 +4,7 @@ pub use chat::Chat;
 pub mod input {
     pub use crate::api::chat::input::*;
 }
+
+pub trait Api<'a> {
+    fn endpoint() -> &'a str;
+}
